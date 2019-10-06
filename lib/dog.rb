017cd22f904fce47e3ad5f02a5cdeb4e4e88f055
tracey brimage <onelovenilynn@gmail.com>
2019-10-06 06:33:00 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+  def bark
+  puts "woof!"
+end
+  end
+  fido = Dog.new
+  fido.bark
+
+  class Dog
+    def sitting
+    puts "The Dog is sitting!"
+  end
+end
+fido=Dog.new
+fido.sitting
+  end
+   end
+  end
---
+++ lib/person.rb
@@ -0,0 +1,16 @@
+class Person
+  def talk
+  puts "Hello World!"
+end
+ end
+ ada= Person.new
+ ada.talk
+
+ end
+ class Person
+   def walk
+   puts "The person is walking"
+ end
+  end
+  ada= Person.new
+  ada.walk
